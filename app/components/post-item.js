6d2589c5c194a991b9638dc4588a1731a76ac2e9
Sean Devine <sean-devine@x-b-e.com>
2018-11-25 18:23:51 -0600

    Move post loading into the component

--- app/components/post-item.js
+++ app/components/post-item.js
@@ -1,5 +1,24 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 export default Component.extend({
   tagName: '',
+
+  init() {
+    this._super(...arguments);
+    if (this.slug) {
+      this.loadPostTask.perform(this.slug);
+    }
+  },
+
+  store: service(),
+
+  loadPostTask: task(function * (slug) {
+    const posts = yield this.store.findAll('post');
+    const post = posts.find((post) => {
+      return post.slug === slug;
+    });
+    this.set('post', post);
+  }),
 });
--- app/routes/posts/show.js
+++ app/routes/posts/show.js
@@ -2,11 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    const { post_slug: slug } = params;
-    return this.store.findAll('post').then((posts) => {
-      return posts.find((post) => {
-        return post.slug === slug;
-      });
-    });
-  }
+    return params.post_slug;
+  },
 });
