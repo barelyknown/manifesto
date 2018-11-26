309b20c2e3a1fba251b6c7dd2cd4621f503a0930
Sean Devine <sean-devine@x-b-e.com>
2018-11-26 01:01:12 -0600

    Fingerprint JSON too?

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,7 +7,7 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
       enabled: env === 'production',
-      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg', 'ico', 'eot', 'ttf', 'woff', 'woff2', 'md', 'csv'],
+      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg', 'ico', 'eot', 'ttf', 'woff', 'woff2', 'md', 'csv', 'json'],
       prepend: process.env.FINGERPRINT_PREPEND || '',
       generateAssetMap: true,
       fingerprintAssetMap: true,
