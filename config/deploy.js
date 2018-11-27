19590385fdbcfca1c51c1fbb1cb5c54253719794
Sean Devine <sean-devine@x-b-e.com>
2018-11-26 23:46:30 -0600

    Add jpg to s3 deploy file pattern

--- config/deploy.js
+++ config/deploy.js
@@ -29,7 +29,7 @@ module.exports = function(deployTarget) {
       region: process.env.REGION,
       filePattern: function(_, pluginHelper) {
         let filePattern = pluginHelper.readConfigDefault('filePattern');
-        return filePattern.replace('}', ',csv,md,json}');
+        return filePattern.replace('}', ',jpg,csv,md,json}');
       },
     };
 
