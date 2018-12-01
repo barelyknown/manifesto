356d2b25f8686ee2e08fdd964147ece6df5eab63
Sean Devine <sean-devine@x-b-e.com>
2018-12-01 16:07:59 -0600

    Only enable fingerprinting in production

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ const env = EmberApp.env();
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      enabled: true,
+      enabled: env === 'production',
       extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'svg', 'ico', 'eot', 'ttf', 'woff', 'woff2', 'md', 'csv', 'json'],
       prepend: process.env.FINGERPRINT_PREPEND || '',
       generateAssetMap: true,
