da6d7a53e69ad1520bea6e36c69ca638561ea5f7
Sean Devine <sean-devine@x-b-e.com>
2018-12-27 04:42:36 -0600

    Refactor post-item component to use passed @post

--- app/components/post-item.js
+++ app/components/post-item.js
@@ -1,23 +1,5 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import { task } from 'ember-concurrency';
 
 export default Component.extend({
   tagName: '',
-
-  init() {
-    this._super(...arguments);
-    if (this.slug) {
-      this.loadPost(this.slug);
-    }
-  },
-
-  store: service(),
-
-  loadPost(slug) {
-    const post = this.store.peekAll('post').find((post) => {
-      return post.slug === slug;
-    });
-    this.set('post', post);
-  }
 });
